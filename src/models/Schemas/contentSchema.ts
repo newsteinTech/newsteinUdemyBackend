a61c3124725a74990979e93f2834cce25de95347
yashwantkrz <Yash.4198@gmail.com>
2019-07-11 02:39:43 +0530

    schema

--- src/models/Schemas/contentSchema.ts
+++ src/models/Schemas/contentSchema.ts
@@ -1,6 +1,10 @@
 import * as mongoose from 'mongoose';
 
 var content = new mongoose.Schema({
+    order : {type: Number},
+    title : {type: String},
+    video : {type: mongoose.SchemaTypes.DocumentArray},
+    subcontent : [{type: mongoose.SchemaTypes.ObjectId, ref:"theContent"}]
 });
 
 export var contentModel = mongoose.model("theContent",content);
