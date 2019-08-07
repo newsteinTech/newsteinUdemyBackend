674bf75316ccccaad683fc74ef921b878ec2ea5d
yashwantkrz <Yash.4198@gmail.com>
2019-08-07 21:57:38 +0530

    Update

--- src/models/Schemas/subscribtionSchema.ts
+++ src/models/Schemas/subscribtionSchema.ts
@@ -1,10 +1,10 @@
 import * as mongoose from 'mongoose';
 
-var subscription = new mongoose.Schema({
+var subscribtion = new mongoose.Schema({
     courseId : {type : String, required:true},
     studentId : {type : String, required:true},
     amount : {type : Number},
     paymentId : {type: String, required:true}
 });
 
-export var contentModel = mongoose.model("theSubscription",subscription);
+export var contentModel = mongoose.model("theSubscription",subscribtion);
